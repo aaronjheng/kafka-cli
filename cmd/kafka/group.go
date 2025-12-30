f55bdc14bb3b7af4e12f0c8ee5c53479374f3715
Aaron Jheng <wentworth@outlook.com>
2025-12-30 02:50:50 +0000

    Refactor group list

--- cmd/kafka/group.go
+++ cmd/kafka/group.go
@@ -15,58 +15,62 @@ func groupCmd() *cobra.Command {
 		Short: "group",
 	}
 
-	cmd.AddCommand(groupListCmd)
+	cmd.AddCommand(groupListCmd())
 	cmd.AddCommand(groupDeleteCmd())
 
 	return cmd
 }
 
-var groupListCmd = &cobra.Command{
-	Use:   "list",
-	Short: "list",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		clusterAdmin, err := newClusterAdmin()
-		if err != nil {
-			return fmt.Errorf("newClusterAdmin error: %w", err)
-		}
-
-		defer func() {
-			if err := clusterAdmin.Close(); err != nil {
-				slog.Error("clusterAdmin.Close failed", slog.Any("error", err))
-			}
-		}()
-
-		groups, err := clusterAdmin.ListConsumerGroups()
-		if err != nil {
-			return fmt.Errorf("clusterAdmin.ListConsumerGroups error: %w", err)
-		}
-
-		consumerGroups := []string{}
-		for k := range groups {
-			consumerGroups = append(consumerGroups, k)
-		}
-
-		details, err := clusterAdmin.DescribeConsumerGroups(consumerGroups)
-		if err != nil {
-			return fmt.Errorf("clusterAdmin.DescribeConsumerGroups error: %w", err)
-		}
-
-		table := tablewriter.NewWriter(os.Stdout)
-		table.Header([]string{"Consumer Group", "State", "Protocol Type", "Protocol", "Members"})
-
-		for _, detail := range details {
-			err := table.Append([]string{detail.GroupId, detail.State, detail.ProtocolType, detail.Protocol, fmt.Sprintf("%d", len(detail.Members))})
+func groupListCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "list",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clusterAdmin, err := newClusterAdmin()
 			if err != nil {
-				return fmt.Errorf("table.Append error: %w", err)
+				return fmt.Errorf("newClusterAdmin error: %w", err)
 			}
-		}
 
-		if err := table.Render(); err != nil {
-			return fmt.Errorf("table.Render error: %w", err)
-		}
+			defer func() {
+				if err := clusterAdmin.Close(); err != nil {
+					slog.Error("clusterAdmin.Close failed", slog.Any("error", err))
+				}
+			}()
 
-		return nil
-	},
+			groups, err := clusterAdmin.ListConsumerGroups()
+			if err != nil {
+				return fmt.Errorf("clusterAdmin.ListConsumerGroups error: %w", err)
+			}
+
+			consumerGroups := []string{}
+			for k := range groups {
+				consumerGroups = append(consumerGroups, k)
+			}
+
+			details, err := clusterAdmin.DescribeConsumerGroups(consumerGroups)
+			if err != nil {
+				return fmt.Errorf("clusterAdmin.DescribeConsumerGroups error: %w", err)
+			}
+
+			table := tablewriter.NewWriter(os.Stdout)
+			table.Header([]string{"Consumer Group", "State", "Protocol Type", "Protocol", "Members"})
+
+			for _, detail := range details {
+				err := table.Append([]string{detail.GroupId, detail.State, detail.ProtocolType, detail.Protocol, fmt.Sprintf("%d", len(detail.Members))})
+				if err != nil {
+					return fmt.Errorf("table.Append error: %w", err)
+				}
+			}
+
+			if err := table.Render(); err != nil {
+				return fmt.Errorf("table.Render error: %w", err)
+			}
+
+			return nil
+		},
+	}
+
+	return cmd
 }
 
 func groupDeleteCmd() *cobra.Command {
