93cb01e610d0260d00736fab8d4a6b9ea02ae16c
Aaron Jheng <wentworth@outlook.com>
2026-04-27 09:25:00 +0800

    Move version commit output to a separate trailing bracket

--- cmd/kafka/version.go
+++ cmd/kafka/version.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"runtime/debug"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -15,12 +16,42 @@ var embeddedVersion string
 
 var version = strings.TrimRight(embeddedVersion, "\n")
 
+func buildCommit() string {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+
+	for _, setting := range buildInfo.Settings {
+		if setting.Key == "vcs.revision" {
+			return setting.Value
+		}
+	}
+
+	return ""
+}
+
 func versionCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Print the version information",
 		Run: func(_ *cobra.Command, _ []string) {
-			_, _ = fmt.Fprintf(os.Stdout, "%s (%s %s/%s)\n", version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+			commit := buildCommit()
+			if commit == "" {
+				_, _ = fmt.Fprintf(os.Stdout, "%s (%s %s/%s)\n", version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+
+				return
+			}
+
+			_, _ = fmt.Fprintf(
+				os.Stdout,
+				"%s (%s %s/%s) (commit: %s)\n",
+				version,
+				runtime.Version(),
+				runtime.GOOS,
+				runtime.GOARCH,
+				commit,
+			)
 		},
 	}
 
